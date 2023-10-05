eade4e1458e9b6b92a927056a8ec109b735c4339
gongzhiyang <374314125@qq.com>
2023-10-05 12:25:39 +0800

    修改第三方配置

--- app/home/config/login.php
+++ app/home/config/login.php
@@ -3,7 +3,7 @@ return [
     "gitee"  => [
         'client_id'    => '544eabc2fa0c121f285257642b6c79419aa4fe3b8604842bbe84f40be1c838b3',
         'client_secret'=> 'af4bbda8df1f809d1d9b03020197cda6aae64a233550063e4a11195cc4368f37',
-        'redirect_uri' =>  'http://api.xiaogongtx.com:8078/home/v1/login/callback'
+        'redirect_uri' =>  'http://xiaogongtx.com/api/v1/login/callback'
     ],
     "github" => []
 
