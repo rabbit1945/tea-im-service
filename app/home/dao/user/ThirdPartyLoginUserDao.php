33f3c42229103cc15702e2c0e36635d339135845
gongzhiyang <374314125@qq.com>
2023-10-07 11:27:16 +0800

    第三方登录

---
+++ app/home/dao/user/ThirdPartyLoginUserDao.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace app\home\dao\user;
+use app\model\ThirdPartyLoginUserModel;
+use app\home\dao\BaseDao;
+
+class ThirdPartyLoginUserDao extends BaseDao
+{
+
+    public function setModel(): string
+    {
+        // TODO: Implement setModel() method.
+        return ThirdPartyLoginUserModel::class;
+
+    }
+
+    /**
+     * 判断是否有这个用户
+     * @param $user_id
+     * @return bool
+     */
+    public function isUser($user_id): bool
+    {
+        if ($this->getModel()->where('id','=',$user_id)->count() > 0)  return true;
+        return false;
+    }
+
+
+
+
+
+}
