f9c5b6f6b2ea1f9ba1641b7c723f1dff745851b5
gongzhiyang <374314125@qq.com>
2023-10-11 18:12:48 +0800

    第三方登录 修改参数

--- app/home/config/login.php
+++ app/home/config/login.php
@@ -2,9 +2,14 @@
 return [
     "gitee"  => [
         "origin" => 'gitee',
-        'client_id'    => 'e80b209e4e9c04bc10899fc2a6679e416a6f36ecc4c0c97c807aeca032a8cae8',
-        'client_secret'=> '6a07357c414e9c8f65b3f5c5195435893d0bd0c0a3850d52626702eda044dfb6',
-        'redirect_uri' =>  'https://1c1892v069.imdo.co/api/v1/login/callback'
+        'client_id'    => '798b9bbd7efdba611d46f4dc0b0be7104c6d9c68f6af8b14239efe6bd43c530f',
+        'client_secret'=> 'd4b2ccc57607475eb6abc6223251651ae365851d254804f45d4abb174015d64a',
+        'redirect_uri' =>  'https://xiaogongtx.com/api/v1/login/callback'
+
+//        'client_id'    => 'e80b209e4e9c04bc10899fc2a6679e416a6f36ecc4c0c97c807aeca032a8cae8',
+//        'client_secret'=> '6a07357c414e9c8f65b3f5c5195435893d0bd0c0a3850d52626702eda044dfb6',
+//        'redirect_uri' =>  'https://1c1892v069.imdo.co/api/v1/login/callback'
+
     ],
     "github" => []
 
